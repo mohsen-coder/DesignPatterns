ae4cfe35cb3bd98047695ac80e7afe8bd6d5429b
Mohsen Falahnejad <rojaweb1@gmail.com>
2023-11-28 08:16:05 +0330

    create(factory.TestFactory): here we test our code.

---
+++ src/main/java/mohsen/coder/factory/TestFactory.java
@@ -0,0 +1,11 @@
+package mohsen.coder.factory;
+
+public class TestFactory {
+    public static void main(String[] args) {
+        Computer pc = ComputerFactory.getComputer("pc","2 GB","500 GB","2.4 GHz");
+        Computer server = ComputerFactory.getComputer("server","16 GB","1 TB","2.9 GHz");
+
+        System.out.println("Factory PC Config::"+pc+" instance of ---> " + pc.getClass());
+        System.out.println("Factory Server Config::"+server+" instance of ---> " + server.getClass());
+    }
+}
